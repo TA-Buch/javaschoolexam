aaef1336542e741f72cb96999c0387f5a9165672
Tatyana <buch_ta@mail.ru>
2019-06-20 16:44:00 +0400

    Fix Pyramiduilder

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,10 +1,8 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+
+import static java.util.Comparator.naturalOrder;
 
 public class PyramidBuilder {
 
@@ -16,31 +14,40 @@ public class PyramidBuilder {
      * @return 2d array with pyramid inside
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
-    public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        LinkedList<Integer> sortedNumbers = inputNumbers.stream()
-                .sorted()
-                .collect(Collectors.toCollection(LinkedList::new));
-        int level = getLevel(sortedNumbers.size());
-        int base = 2 * level - 1;
-        int[][] pyramid = new int[level][base];
-        Iterator<Integer> sortNumIter = sortedNumbers.iterator();
-        for (int i = 0; i < level; i++) {
-            int startPos = level - (i + 1);
-            int stopPos = level + i;
-            for (int j = startPos; j <= stopPos; j += 2) {
-                pyramid[i][j] = sortNumIter.next();
+    public int[][] buildPyramid(List<Integer> inputNumbers) throws CannotBuildPyramidException {
+        try {
+            List<Integer> sortedInputNumbers = new ArrayList<>(inputNumbers);
+            sortedInputNumbers.sort(Comparator.nullsFirst(Comparator.naturalOrder()));
+            int level = getPyramidHeight(sortedInputNumbers.size());
+            int width = 2 * level - 1;
+            int[][] pyramid = new int[level][width];
+            Iterator<Integer> sortNumIter = sortedInputNumbers.iterator();
+            for (int i = 0; i < level; i++) {
+                int startPos = level - (i + 1);
+                int stopPos = level + i;
+                for (int j = startPos; j <= stopPos; j += 2) {
+                    pyramid[i][j] = sortNumIter.next();
+                }
             }
+            return pyramid;
         }
-        return pyramid;
+        catch (Throwable e) {
+            throw new CannotBuildPyramidException();
+        }
     }
 
-    private int getLevel(int sizeList) {
-        int levelPyramid = 0;
-        for(int i =1; i <= sizeList; i += levelPyramid) {
-            levelPyramid++;
+    private static int getPyramidHeight(int numberOfElements) throws CannotBuildPyramidException {
+        int height = 0;
+        int i;
+        for (i = 1; i <= numberOfElements; i += height) {
+            height++;
         }
-        return levelPyramid;
+        System.out.println(i);
+        System.out.println(height);
+        if (i > numberOfElements+1) {
+            throw new CannotBuildPyramidException();
+        }
+        return height;
     }
 
 }
